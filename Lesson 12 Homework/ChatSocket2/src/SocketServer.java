fe0b7d186c7b2d903205ea588cd9cba4b16353fa
dtranyl <dtranyl85@gmail.com>
2023-05-20 02:33:49 -0700

    Lesson 12 homework

---
+++ Lesson 12 Homework/ChatSocket2/src/SocketServer.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SocketServer {
+    private static List<Socket> clients = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+
+            // Khởi tạo ServerSocket với cổng số 5000
+            ServerSocket serverSocket = new ServerSocket(5000);
+            System.out.println("Server is running...");
+
+            while (true) {
+                // Chấp nhận kết nối từ Socket Client
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket);
+                clients.add(socket);
+                // Tạo luồng riêng cho kết nối đó
+                ClientHandler clientHandler = new ClientHandler(socket);
+                // Khởi động luồng
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+    }
+
+    // Class ClientHandler thực hiện xử lý dữ liệu của một Socket Client
+    private static class ClientHandler implements Runnable {
+        private final Socket socket;
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+        @Override
+        public void run() {
+            try {
+                // Tạo luồng đọc dữ liệu từ Socket Client
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                // Đọc và ghi dữ liệu liên tục
+                while(true) {
+                    String receivedData = reader.readLine();
+                    if (receivedData != null) {
+                        System.out.println("Received from client " + socket + ": " + receivedData);
+                        SendMessage(receivedData);
+                    }
+                }
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    // Đóng kết nối khi kết thúc luồng
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                clients.remove(socket);
+            }
+        }
+
+        private void SendMessage(String message) throws IOException {
+            for (Socket client : clients) {
+                if(client !=socket){
+                    PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+                    writer.println("Server received data from clients:" + message);
+                }
+            }
+        }
+    }
+
+}
+
